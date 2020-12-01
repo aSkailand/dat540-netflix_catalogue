179213c9c36ac9853cfbebcc93179be2cd02ab20
aleksbj96@gmail.com <aleksbj96@gmail.com>
2020-12-01 15:32:34 +0100

    Heatmap explring genre vs years and months

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import pandas
 import data_sanitizer
 import get_unique
-import popular_genre
+import yrs_months
+#import popular_genre
 
 netflix_titles_filepath = 'netflix_titles.csv'
 
@@ -16,7 +17,11 @@ if __name__ == "__main__":
     data_sanitizer.handle_null_values(data_set) 
     unique_genres = get_unique.genres(data_set)
     unique_countries = get_unique.countires(data_set)
-
-
+    years_movies = get_unique.get_years(data_set,"Movie")
+    #years = yrs_months.all_years(data_set)
+    #print(years)
+    #dates_netflix = get_unique.get_months_years(data_set)
+    #print(years_movies)
+    print(unique_genres)
 
 

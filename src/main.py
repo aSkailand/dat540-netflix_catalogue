6f217a46cda8ab21b8777216a3b92e57abc5e986
Aslak Frafjord Skailand <aslakfrafjord.skailand@altibox.no>
2020-12-03 08:51:25 +0100

    analyzing gener representation.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import pandas
 import data_sanitizer
 import get_unique
-import popular_genre
+import gender_analysis
 
 netflix_titles_filepath = 'netflix_titles.csv'
+names_gender_filepath = 'name_gender.csv'
 
 def get_netflix_catalogue_dataframe():
     '''
@@ -11,12 +12,19 @@ def get_netflix_catalogue_dataframe():
     '''
     return pandas.DataFrame(pandas.read_csv(netflix_titles_filepath))
 
+def get_names_dataframe():
+    '''
+        Return names as a pandas dataframe.
+    '''
+    return pandas.DataFrame(pandas.read_csv(names_gender_filepath))
+
 if __name__ == "__main__":
     data_set =  get_netflix_catalogue_dataframe()
     data_sanitizer.handle_null_values(data_set) 
     unique_genres = get_unique.genres(data_set)
     unique_countries = get_unique.countires(data_set)
 
-
+    names = get_names_dataframe()
+    gender_analysis.analyse(names, data_set)
 
 
